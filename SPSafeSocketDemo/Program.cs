8d09ed92524ccb2d30f2730a3d14d03f1d28eb72
Junian Triajianto <author@junian.net>
2018-09-06 11:16:56 +0700

    Add new project

---
+++ SPSafeSocketDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SPSafeSocketDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
