23ee56b747ad832d87e0666e1d2ff9476341845c
Junian Triajianto <author@junian.net>
2018-09-06 16:07:52 +0700

    Add looping to random items

--- SPSafeSocketDemo/Program.cs
+++ SPSafeSocketDemo/Program.cs
@@ -5,30 +5,29 @@ namespace SPSafeSocketDemo
 {
     class Program
     {
+        // Change it to your Sharepoint list URL
         static readonly string ListURL = "http://example.com/list";
 
         static void Main(string[] args)
         {
             // Change to larger loop number if you don't see SocketException
-            var loop = 100;
+            var loop = 256;
 
             for (var i = 0; i < loop; i++)
             {
                 // Uncomment one of these.
-                //
+                // * * *
                 // 1. Normal execution, without catching SocketException.
-                //ExecuteNormally(i);
-
+                // ExecuteNormally(i);
+                // * * *
                 // 2. Catch SocketException and see the error code,
                 //    if it's a WSAEADDRINUSE error, try again.
-                ExecuteSafely(i);
+                   ExecuteSafely(i);
             }
         }
 
         static void ExecuteNormally(int batch)
         {
-            // Starting with ClientContext, the constructor requires a 
-            // URL to the server running SharePoint. 
             using (var context = new ClientContext(ListURL))
             {
                 AddRandomItems(context, batch);
@@ -48,15 +47,18 @@ namespace SPSafeSocketDemo
 
         static void AddRandomItems(ClientContext ctx, int batch)
         {
-            // Assume that the web has a list named "Announcements". 
-            var announcementsList = ctx.Web.Lists.GetByTitle("Announcements");
+            for (var i = 0; i < 100; i++)
+            {
+                // Assume that the web has a list named "Announcements". 
+                var announcementsList = ctx.Web.Lists.GetByTitle("Announcements");
 
-            // We are just creating a regular list item.
-            var itemCreateInfo = new ListItemCreationInformation();
-            var newItem = announcementsList.AddItem(itemCreateInfo);
-            newItem["Title"] =
-                $"Batch #{batch}, Item #{DateTime.UtcNow.ToString("R")}";
-            newItem.Update();
+                // We are just creating a regular list item.
+                var itemCreateInfo = new ListItemCreationInformation();
+                var newItem = announcementsList.AddItem(itemCreateInfo);
+                newItem["Title"] =
+                    $"Batch #{batch}, Item #{DateTime.UtcNow.ToString("R")}";
+                newItem.Update();
+            }
         }
     }
 }
